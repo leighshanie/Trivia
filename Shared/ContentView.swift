9ef052b230bf60830f016981056db89b64e92f9b
Shanie Leigh <yao.h.wang@uni.sydney.edu.au>
2020-07-08 01:24:42 +0800

    add timer and active detection

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -19,10 +19,27 @@ struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     
     @State private var cards = [Card](repeating: Card.example, count: 10)
+    @State private var isActive = true
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         ZStack {
             VStack {
+                HStack {
+                    Spacer()
+                    Text("Time: \(timeRemaining)")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 5)
+                        .background(
+                            Capsule()
+                                .fill(Color.black)
+                                .opacity(0.75)
+                        )
+                }
+                    
                 ZStack {
                     ForEach(0..<cards.count, id:\.self) { index in
                         CardView(card: self.cards[index]) {
@@ -58,8 +75,18 @@ struct ContentView: View {
                 }
             }
         }
-        
-        
+        .onReceive(timer) { time in
+            guard self.isActive else { return }
+            if self.timeRemaining > 0 {
+                self.timeRemaining -= 1
+            }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+            self.isActive = false
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+            self.isActive = true
+        }
     }
     
     func removeCard(at index: Int) {
