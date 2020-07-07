236bcb9e53b79d55f2c6a4db2a2f0f09d868fca9
Shanie Leigh <yao.h.wang@uni.sydney.edu.au>
2020-07-08 00:36:13 +0800

    add stacked cardview

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,64 +7,31 @@
 
 import SwiftUI
 
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = CGFloat(total - position)
+        return self.offset(CGSize(width: -offset * 2.5, height: -offset * 5))
+    }
+}
 struct ContentView: View {
-    @State private var currentAmount: CGFloat = 0
-    @State private var finalAmount: CGFloat = 1
     
-    @State private var currentAngle: Angle = .degrees(0)
-    @State private var finalAngle: Angle = .degrees(0)
-
+    
+    @State private var cards = [Card](repeating: Card.example, count: 10)
     
     var body: some View {
-        
-        VStack {
-
-            combinedGesture()
-
-            Text("Double Tap")
-                .onTapGesture(count: 2) {
-                    print("Double tapped!")
+        ZStack {
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id:\.self) { index in
+                        CardView(card: self.cards[index])
+                            .stacked(at: index, in: self.cards.count)
+                    }
+//                    CardView(card: Card.example)
                 }
-                .padding()
-
-            Text("Long press")
-                .onLongPressGesture(minimumDuration: 2) {
-                    print("Long pressed!")
-                }
-                .background(Color.black)
-                .foregroundColor(.white)
-                .padding()
-
-            Text("Magnification Gesture!")
-                .scaleEffect(finalAmount + currentAmount)
-                .gesture(
-                    MagnificationGesture()
-                        .onChanged { amount in
-                            self.currentAmount = amount
-                        }
-                        .onEnded { amount in
-                            self.finalAmount += self.currentAmount
-                            self.currentAmount = 0
-                        }
-                )
-                .padding()
-
-            Text("Rotation Gesture!")
-                .rotationEffect(finalAngle + currentAngle)
-                .gesture(
-                    RotationGesture()
-                        .onChanged { angle in
-                            self.currentAngle = angle
-                        }
-                        .onEnded { angle in
-                            self.finalAngle += self.currentAngle
-                            self.currentAngle = .degrees(0)
-                        }
-                )
-                .padding()
-
-
+            }
         }
+        
+        
     }
 }
 
